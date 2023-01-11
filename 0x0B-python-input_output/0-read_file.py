9aca395886253e4ac9045b6ebc56a099f440894b
nwajude <nwabuddikejude1@gmail.com>
2023-01-10 17:29:06 -0800

    err

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Module: 0-read_file
-This is a function that reads a text file (UTF8) and prints it to stdout
 """
-def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout
-    Args:
-        - filename: this is the file to be read
-    """   
+function that reads a text file
+"""
 
-    with open(filename) as f:
-        read_data = f.read()
-        print(read_data, end="")
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Module: 1-write_file
-This is a function that writes a string to a text file (UTF8) and returns the number of characters written
+"""
+function that writes a string to a text file (UTF8) and returns the number of characters written
 """
 
 
 def write_file(filename="", text=""):
-    """This function writes a string to a text file
-    Args:
-        - filename: the name of the file that is to be write to
-        - text: the string to be added to the file
+    """ module write_file
     """
-
-    with open(filename, 'w+') as f:
+    with open(filename, 'w') as f:
         return f.write(text)
